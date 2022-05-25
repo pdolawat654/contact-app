8decede3d4c7b6c19c4c94a4845fe00c4852748d
pdolawat654 <pdolawat654@gmail.com>
2022-05-25 21:49:31 +0530

    24/05/2022

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import React, {useState} from 'react';
+import Header from "./Header"
+import AddContact from "./AddContact"
+import ContactList from "./ContactList"
 
 function App() {
+  const [contacts, setContacts] = useState([])
+  const addContactHandler = (contact) => {
+    console.log(contact)
+    setContacts([...contacts, contact])
+  }
   return (
-    <div>Hello World</div>
+    <div className='ui container'>
+      <Header />
+      <AddContact addContactHandler={addContactHandler}/>
+      <ContactList contacts={contacts}/>
+    </div>
   );
 }
 
